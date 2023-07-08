f29be0c3f65e90da2a38beb2273ce789dff8371c
JoBiden <blazierg@gmail.com>
2023-07-08 01:20:39 -0700

    Update repo from 4.5 to 4.8

---
+++ FChatDicebot/FChatDicebot/BotCommands/Help - Copy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FChatDicebot.BotCommands.Base;
+
+namespace FChatDicebot.BotCommands
+{
+    public class Alive : ChatBotCommand
+    {
+        public Alive()
+        {
+            Name = "alive";
+            RequireBotAdmin = false;
+            RequireChannelAdmin = false;
+            RequireChannel = false;
+            LockCategory = CommandLockCategory.NONE;
+        }
+
+        public override void Run(BotMain bot, BotCommandController commandController, string[] rawTerms, string[] terms, string characterName, string channel, UserGeneratedCommand command)
+        {
+            string messageText = "Yes, the Chateau is ALIVE! Always listening, always watching... in kind of a pervy way, to be honest.";
+
+            if (!commandController.MessageCameFromChannel(channel))
+            {
+                bot.SendPrivateMessage(messageText + "\n\nThank you for checking on the Chateau, it means a lot ♥", characterName);
+            }
+            else
+            {
+                bot.SendMessageInChannel(messageText, channel);
+            }
+        }
+    }
+}
